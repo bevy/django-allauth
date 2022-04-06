9c72d830ce864d8f57e31b64227820a0f7cc299c
Gabrielle Singh Cadieux <gabrielle.singhcadieux@bevy.com>
2022-04-06 18:29:26 -0400

    feat(oauth2): support plain code challenge method

--- allauth/socialaccount/providers/oauth2/utils.py
+++ allauth/socialaccount/providers/oauth2/utils.py
@@ -4,14 +4,17 @@ import random
 from secrets import token_urlsafe
 
 
-def generate_code_challenge():
+def generate_code_challenge(code_challenge_method="S256"):
     # minimum length of 43 characters, maximum length of 128 characters
     nbytes = random.randint(43, 128)
     code_verifier = token_urlsafe(nbytes)
-    hashed_verifier =  hashlib.sha256(code_verifier.encode("ascii"))
-    code_challenge = base64.urlsafe_b64encode(hashed_verifier.digest())
+    if code_challenge_method == "plain":
+        code_challenge = code_verifier
+    else:
+        hashed_verifier = hashlib.sha256(code_verifier.encode("ascii"))
+        code_challenge = base64.urlsafe_b64encode(hashed_verifier.digest())
     return {
         "code_verifier": code_verifier,
-        "code_challenge_method": "S256",
+        "code_challenge_method": code_challenge_method,
         "code_challenge": code_challenge
     }
