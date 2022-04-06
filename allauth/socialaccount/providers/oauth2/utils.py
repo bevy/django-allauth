c3b8039e4c6d738ef33d16380f4c88bcdaa6b2f3
Gabrielle Singh Cadieux <gabrielle.singhcadieux@bevy.com>
2022-04-06 18:32:36 -0400

    Revert "feat(oauth2): support plain code challenge method"
    This reverts commit 0ab5ac5468f54b1ecc3ebb7b9db7a3f0052a2611.

--- allauth/socialaccount/providers/oauth2/utils.py
+++ allauth/socialaccount/providers/oauth2/utils.py
@@ -4,17 +4,14 @@ import random
 from secrets import token_urlsafe
 
 
-def generate_code_challenge(code_challenge_method="S256"):
+def generate_code_challenge():
     # minimum length of 43 characters, maximum length of 128 characters
     nbytes = random.randint(43, 128)
     code_verifier = token_urlsafe(nbytes)
-    if code_challenge_method == "plain":
-        code_challenge = code_verifier
-    else:
-        hashed_verifier = hashlib.sha256(code_verifier.encode("ascii"))
-        code_challenge = base64.urlsafe_b64encode(hashed_verifier.digest())
+    hashed_verifier =  hashlib.sha256(code_verifier.encode("ascii"))
+    code_challenge = base64.urlsafe_b64encode(hashed_verifier.digest())
     return {
         "code_verifier": code_verifier,
-        "code_challenge_method": code_challenge_method,
+        "code_challenge_method": "S256",
         "code_challenge": code_challenge
     }
