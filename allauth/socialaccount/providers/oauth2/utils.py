6663bc873117839185ffc1cea0b857ca3d488b44
Gabrielle Singh Cadieux <gabrielle.singhcadieux@bevy.com>
2022-04-19 13:32:58 -0400

    fix(oauth2): remove code_challenge padding

--- allauth/socialaccount/providers/oauth2/utils.py
+++ allauth/socialaccount/providers/oauth2/utils.py
@@ -22,8 +22,9 @@ def generate_code_challenge():
     code_verifier = token_urlsafe(nbytes)
     hashed_verifier = hashlib.sha256(code_verifier.encode("ascii"))
     code_challenge = base64.urlsafe_b64encode(hashed_verifier.digest())
+    code_challenge_without_padding = code_challenge.rstrip(b"=")
     return {
         "code_verifier": code_verifier,
         "code_challenge_method": "S256",
-        "code_challenge": code_challenge,
+        "code_challenge": code_challenge_without_padding,
     }
